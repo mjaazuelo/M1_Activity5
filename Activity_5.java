afdf38094fdea091fadbe2e38adec8a81fff501f
Maica Josette A. Azuelo <mjaazuelo@headoffice.corp.bpi.com.ph>
2025-12-02 16:10:15 +0800

    Initial commit

---
+++ Activity_5.java
@@ -0,0 +1,14 @@
+package Activity_5;
+
+public class Activity_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+        for (int x = 0; x <= 50; x++) {
+            sum += x;
+        } // end of for loop
+        System.out.println(sum); // Display sum of numbers 1-50
+	}//end of main method
+
+}
